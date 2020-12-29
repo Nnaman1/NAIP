fbfe1d4ce514af018e341a53d26ad9b6be96c796
Nnaemeka <N.nnaemeka@gmail.com>
2020-12-29 12:08:44 -0600

    site update

--- js/plugins.js
+++ js/plugins.js
@@ -12,7 +12,6 @@ function checkScroll() { //check if page is scrolled
     }
 }
 
-
 /*========== ADD SOLID CLASS TO NAVBAR WHEN TOGGLED ==========*/
 $('.navbar-toggler').click(function () { //when navbar-toggler is clicked
     if ($(window).scrollTop() <= 300) { //if window scrolled 300px or less from top
@@ -36,7 +35,6 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     }, 1000);
 });
 
-/*========== BOUNCING DOWN ARROW ==========*/
 $(document).ready(function () {
     $(window).scroll(function () {
         $('.arrow').css('opacity', 1 - $(window).scrollTop() / 250);
@@ -44,7 +42,16 @@ $(document).ready(function () {
 });
 
 
-/*========== LIGHTBOX IMAGE GALLERY ==========*/
+$(document).ready(function () {
+    $(document).click(function (event) {
+        var click = $(event.target);
+        var _open = $(".navbar-collapse").hasClass("show");
+        if (_open === true && !click.hasClass("navbar-toggler")) {
+            $(".navbar-toggler").click();
+        }
+    });
+});
+
 $(document).ready(function () {
     lightbox.option({
         'resizeDuration': 600,
@@ -53,62 +60,6 @@ $(document).ready(function () {
     });
 });
 
-
-/*========== MEET THE TEAM CAROUSEL ==========*/
-$(document).ready(function () { //when document(DOM) loads completely
-    $('#team-carousel').owlCarousel({ //owlCarousel settings
-        autoplay: true, //set to false to turn off autoplay and only use nav
-        autoplayHoverPause: true, //set to false to prevent pausing on hover
-        loop: true, //set to false to stop carousel after all slides shown
-        autoplayTimeout: 8000, //time between transitions
-        smartSpeed: 1200, //transition speed
-        dotsSpeed: 1000, //transition speed when using dots/buttons
-        responsive: { //set number of items shown per screen width
-            0: {
-                items: 1 //0px width and up display 1 item
-            },
-            768: {
-                items: 2 //768px width and up display 2 items
-            },
-            992: {
-                items: 3 //992px width and up display 3 items
-            }
-        }
-    });
-});
-
-
-/*========== SKILLS COUNTER ==========*/
-$(document).ready(function () {
-    $('.counter').counterUp({
-        delay: 10,
-        time: 3000,
-        beginAt: 0
-    });
-});
-
-
-/*========== CLIENTS CAROUSEL ==========*/
-$(document).ready(function () { //when document(DOM) loads completely
-    $('#clients-carousel').owlCarousel({ //owlCarousel settings
-        autoplay: true, //set to false to turn off autoplay and only use nav
-        autoplayHoverPause: true, //set to false to prevent pausing on hover
-        loop: true, //set to false to stop carousel after all slides shown
-        autoplayTimeout: 8000, //time between transitions
-        smartSpeed: 1600, //transition speed
-        dotsSpeed: 1000, //transition speed when using dots/buttons
-        responsive: { //set number of items shown per screen width
-            0: {
-                items: 1 //0px width and up display 1 item
-            },
-            768: {
-                items: 2 //768px width and up display 2 items
-            }
-        }
-    });
-});
-
-/*========== TOP SCROLL BUTTON ==========*/
 $(document).ready(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 500) {
@@ -119,8 +70,6 @@ $(document).ready(function () {
     });
 });
 
-
-/*========== MAKE ALL ANIMATION "FADEINUP" ON MOBILE ==========*/
 $(document).ready(function () {
     if ($(window).width() < 768) {
         $('div').attr('data-animation', 'fadeInUp');
@@ -128,10 +77,6 @@ $(document).ready(function () {
     }
 });
 
-
-
-/*========== WAYPOINTS ANIMATION DELAY ==========*/
-//Original Resource: https://www.oxygenna.com/tutorials/scroll-animations-using-waypoints-js-animate-css
 $(function () { // a self calling function
     function onScrollInit(items, trigger) { // a custom made function
         items.each(function () { //for every element in items run function
@@ -160,50 +105,3 @@ $(function () { // a self calling function
     onScrollInit($('.staggered-animation'), $('.staggered-animation-container')); //function call with items and trigger
 });
 
-
-/*========== CONTACT FORM INPUT VALIDATION ==========*/
-//Original Resource: https://bootstrapious.com/p/how-to-build-a-working-bootstrap-contact-form
-$(function () {
-
-    // init the validator
-    // validator files are included in the download package
-    // otherwise download from http://1000hz.github.io/bootstrap-validator
-
-    $('#contact-form').validator();
-
-
-    // when the form is submitted
-    $('#contact-form').on('submit', function (e) {
-
-        // if the validator does not prevent form submit
-        if (!e.isDefaultPrevented()) {
-            var url = "contact/contact.php";
-
-            // POST values in the background the the script URL
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data) {
-                    // data = JSON object that contact.php returns
-
-                    // we recieve the type of the message: success x danger and apply it to the
-                    var messageAlert = 'alert-' + data.type;
-                    var messageText = data.message;
-
-                    // let's compose Bootstrap alert box HTML
-                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-
-                    // If we have messageAlert and messageText
-                    if (messageAlert && messageText) {
-                        // inject the alert to .messages div in our form
-                        $('#contact-form').find('.messages').html(alertBox);
-                        // empty the form
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
-        }
-    })
-});
